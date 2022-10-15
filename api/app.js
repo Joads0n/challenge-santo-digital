acc1d3caeddd3abf182e6f911a35a6115ccc2e7d
Joadson Silva <joadson83@hotmail.com>
2022-10-15 10:36:02 -0300

    configuração inicial do servidor

--- api/app.js
+++ api/app.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const routes = require('./routes/app');
+
+app = express();
+const port = 3000;
+
+routes(app);
+
+app.listen(port, () => {
+    console.log(`SEVER ON PORT: ${port}`)
+});
+
+module.exports = app;
 
 
---
+++ api/routes/app.js
@@ -0,0 +1,7 @@
+const bodyParser = require('body-parser');
+
+module.exports = app => {
+    app.get('/', (req, res) => {
+        res.send('Hello World');
+    })
+}
