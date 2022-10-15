2048a8efe3ef2221321def014c85dcc77dd15f9a
Joadson Silva <joadson83@hotmail.com>
2022-10-15 12:00:26 -0300

    Ajuste no nome das tabelas

--- api/migrations/20221015145257-create-prof.js
+++ api/migrations/20221015145257-create-prof.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Profs', {
+    await queryInterface.createTable('prof', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -26,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Profs');
+    await queryInterface.dropTable('prof');
   }
 };
