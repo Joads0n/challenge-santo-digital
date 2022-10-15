b2a7d1726b497aa01a99bf7aa8d88195115b9ef4
Joadson Silva <joadson83@hotmail.com>
2022-10-15 12:14:15 -0300

    Altaração do nome da tabela registration

--- api/migrations/20221015151246-create-registration.js
+++ api/migrations/20221015151246-create-registration.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Registrations', {
+    await queryInterface.createTable('registration', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -26,6 +26,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Registrations');
+    await queryInterface.dropTable('registration');
   }
 };
